03852d3c4b59ddeb370d85be482c14410cdffd13
pattysc <pattys@users.noreply.github.com>
2017-01-02 00:18:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :school_classes
+  resources :students
+
   # Example resource route with options:
   #   resources :products do
   #     member do
